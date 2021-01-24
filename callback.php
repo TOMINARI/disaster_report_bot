cf127ac4f69a423daf20ff8053c7049b74231fe6
tominari <tominari.takayuki@gmail.com>
2021-01-24 13:25:29 +0900

    dev16

--- callback.php
+++ callback.php
@@ -63,6 +63,7 @@ $target_sheetname = GetTargetSheetName( $spreadsheetId);
  $log->addWarning("target_sheet  ${target_sheetname}\n");
 
 foreach ($events as $event) {
+  $bot->replyText($event);
   if($event->getText() == "たすけて！")
   {
     $bot->replyText($event->getReplyToken(), "助けを呼びます！位置情報を送信してください。");
