efd80b48cdc979e1c4df417773c190610e34937d
tominari <tominari.takayuki@gmail.com>
2021-01-24 12:29:38 +0900

    dev12

--- callback.php
+++ callback.php
@@ -511,9 +511,9 @@ foreach ($events as $event) {
             [
               "type"=>"location",
               "title"=>"助けて！が届いています",
-              "address"=>"ここです",
-              "latitude"=>35.688806,
-              "longitude"=>139.701739,
+              "address"=>"",
+              "latitude"=>$latitude,
+              "longitude"=>$longitude
             ]
         ]
       );
